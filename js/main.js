1d3703a2fc5434d5879c960ff6461e9e4cdfd984
Artyom Prysiazhnyy <77559081+ArtyomPrisyazhnyy@users.noreply.github.com>
2021-10-01 02:57:53 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const burger = document.querySelector(".burger");
+const container = document.querySelector(".container");
+const bar = document.querySelector(".bar");
+const screens = document.querySelectorAll(".screen");
+
+burger.addEventListener("click", () => {
+    container.classList.toggle("active");
+    bar.classList.toggle("active");
+});
+
+function replaceBg(id){
+    const bg = document.getElementById(id);
+    screens.forEach(screen => {
+        screen.style.display = "none";
+    })
+    bg.style.display = "block";
+}
+
+function changeBg(){
+    const links = document.querySelectorAll(".link");
+
+    links.forEach(link => {
+        link.addEventListener("mouseenter", e => {
+            e.preventDefault();
+            replaceBg(e.target.dataset.link);
+        })
+
+        link.addEventListener("click", e => {
+            e.preventDefault();
+            container.classList.toggle("active");
+        })
+    });
+
+    screens.forEach(screen => {
+        screen.style.display = "none";
+        screens[0].style.display = "block";
+        
+        screen.addEventListener("click", e => {
+            e.preventDefault();
+            container.classList.remove("active");
+        })
+    })
+};
+changeBg();
